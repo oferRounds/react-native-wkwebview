b778cc881a74ae6ab9f587dc624431b4ebbe8a8e
ofer morag <ofer.morag@hibob.io>
2018-11-28 09:12:22 +0200

    Removed the usage of `suppressesIncrementalRendering`

--- ios/RCTWKWebView/CRAWKWebView.m
+++ ios/RCTWKWebView/CRAWKWebView.m
@@ -71,8 +71,6 @@ RCT_NOT_IMPLEMENTED(- (instancetype)initWithCoder:(NSCoder *)aDecoder)
     
     WKWebViewConfiguration* config = [[WKWebViewConfiguration alloc] init];
     config.processPool = processPool;
-    config.suppressesIncrementalRendering = YES;
-
     
     
     WKUserContentController *userController = [WKUserContentController new];
